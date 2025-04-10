9d133f3a8c1bf2d3a2e3ecc5eaec037a3e5e07b3
Tuannghia235 <162992610+Tuannghia235@users.noreply.github.com>
2025-04-10 21:36:18 +0700

    init commit

--- WebsiteBanAoPolo-main/src/main/java/org/example/registerpolo/Repository/HoaDonRepo.java
+++ WebsiteBanAoPolo-main/src/main/java/org/example/registerpolo/Repository/HoaDonRepo.java
@@ -5,17 +5,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.lang.NonNull;
 
 import java.time.LocalDate;
 
 
 @Repository
 public interface HoaDonRepo extends JpaRepository<HoaDon, Integer> {
-    Page<HoaDon> findAll(Pageable pageable);
+    @NonNull
+    Page<HoaDon> findAll(@NonNull Pageable pageable);
     // Tìm kiếm hóa đơn theo khoảng thời gian
-    Page<HoaDon> findByKhachHangSdtContaining(String sdt, Pageable pageable);
+    Page<HoaDon> findByKhachHangSdtContaining(String sdt, @NonNull Pageable pageable);
 
-    Page<HoaDon> findByNgayMuaHangBetween(LocalDate startDate, LocalDate endDate, Pageable pageable);
+    Page<HoaDon> findByNgayMuaHangBetween(LocalDate startDate, LocalDate endDate, @NonNull Pageable pageable);
 
     // Không cần thêm findById vì JpaRepository đã có sẵn
 
